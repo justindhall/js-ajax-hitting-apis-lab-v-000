8dbfa487573f7874cd480026b79e4114f983331b
Justin Hall <hall.justin.d@gmail.com>
2019-01-14 23:34:21 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // your code here
+
+function getRepositories(){
+  //get username value
+  const username =
+  const req = new XMLHttpRequest();
+  
+  req.addEventListener('load', showRepositories);
+  
+  req.open('GET' /repos/:owner/:repo)
+}
