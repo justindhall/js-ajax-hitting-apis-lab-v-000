f59a3031b5262438300e57a72636199bd0c6836a
Justin Hall <hall.justin.d@gmail.com>
2019-01-14 23:38:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,9 +3,14 @@
 function getRepositories(){
   //get username value
   const username = document.getElementById("username").value
+  
+  //xmlRequest
   const req = new XMLHttpRequest();
   
+  //listener for shower to act on
   req.addEventListener('load', showRepositories);
   
-  const url = `https://
+  const url = `https://api.github.com/users/${username}/repos`
+  
+  req.open('GET', url)
 }
