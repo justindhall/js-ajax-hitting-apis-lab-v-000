463e4117bc3413f0a4d84a4512b2ffb5e080c59c
Justin Hall <hall.justin.d@gmail.com>
2019-01-14 23:39:55 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,7 +10,13 @@ function getRepositories(){
   //listener for shower to act on
   req.addEventListener('load', showRepositories);
   
+  //abstracted link to url
   const url = `https://api.github.com/users/${username}/repos`
   
+  //open with abstracted url
   req.open('GET', url)
+  
+  //send request 
+  req.send()
 }
+
